1c1498e42e91437b7eb79ae8282701939c023d83
grea-t <1193976273@qq.com>
2020-06-23 23:06:37 +0800

    提交

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="popover" @click.stop="xxx">
-    <div class="content-wrapper" v-if="visible" @click.stop>
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
-    <slot></slot>
+    <span ref="triggerWrapper">
+      <slot></slot>
+    </span>
   </div>
 </template>
 
@@ -18,6 +20,10 @@
         this.visible = !this.visible
         if (this.visible === true) {
           this.$nextTick(() => {
+            document.body.appendChild(this.$refs.contentWrapper)
+            let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+            this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+            this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
             let eventHandler = () => {
               this.visible = false
               document.removeEventListener('click', eventHandler)
@@ -26,7 +32,7 @@
           })
         }
       }
-    }
+    },
   }
 </script>
 
@@ -35,11 +41,10 @@
     display: inline-block;
     vertical-align: top;
     position: relative;
+  }
 
-    .content-wrapper {
-      position: absolute;
-      bottom: 100%;
-      left: 0;
-    }
+  .content-wrapper {
+    position: absolute;
+    transform: translateY(-100%);
   }
 </style>
