3fd784be96a72d170b044e6492451b01d03d4c9c
grea-t <1193976273@qq.com>
2020-07-06 21:54:07 +0800

    提交

--- src/button.vue
+++ src/button.vue
@@ -1,93 +1,93 @@
 <template>
-    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')">
-        <g-icon class="icon" v-if="icon &&!loading" :name="icon"></g-icon>
-        <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
-        <div class="content">
-            <slot></slot>
-        </div>
-    </button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')">
+    <g-icon class="icon" v-if="icon &&!loading" :name="icon"></g-icon>
+    <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
-    import Icon from './icon'
-    export default {
-        name:'GuluButton',
-        components:{
-            'g-icon':Icon
-        },
-        props: {
-            icon: {},
-            loading: {
-                type: Boolean,
-                default: false
-            },
-            iconPosition: {
-                type: String,
-                default: "left",
-                validator(value) {
-                    return !(value === "left" && value === "right")
-                }
-            }
+  import Icon from './icon'
+
+  export default {
+    name: 'GuluButton',
+    components: {
+      'g-icon': Icon
+    },
+    props: {
+      icon: {},
+      loading: {
+        type: Boolean,
+        default: false
+      },
+      iconPosition: {
+        type: String,
+        default: "left",
+        validator(value) {
+          return !(value === "left" && value === "right")
         }
-    };
+      }
+    }
+  };
 </script>
 <style lang="scss" scoped>
-    @keyframes spin {
-        0% {
-            transform: rotate(0deg);
-        }
-        100% {
-            transform: rotate(360deg);
-        }
+  $font-size: 14px;
+  $button-height: 32px;
+  $button-bg: white;
+  $button-active-bg: #eee;
+  $border-radius: 4px;
+  $color: #333;
+  $border-color: #999;
+  $border-color-hover: #666;
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
     }
-
-    .g-button {
-        font-size: var(--font-size);
-        height: var(--button-height);
-        padding: 0 1em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-bg);
-        vertical-align: middle;
-        display: inline-flex;
-        justify-content: center;
-        align-content: center;
-
-        &:hover {
-            border-color: var(--border-color-hover);
-        }
-
-        &:active {
-            background-color: var(--button-active-bg);
-        }
-
-        &:focus {
-            outline: none;
-        }
-
-        > .icon {
-            order: 1;
-            margin-right: 0.3em;
-        }
-
-        > .content {
-            order: 2;
-            line-height: 15px;
-        }
-
-        &.icon-right {
-            > .icon {
-                order: 2;
-                margin-right: 0;
-                margin-left: 0.3em;
-            }
-
-            > .content {
-                order: 1;
-            }
-        }
-
-        .loading {
-            animation: spin 2s infinite linear;
-        }
+    100% {
+      transform: rotate(360deg);
     }
+  }
+  .g-button {
+    font-size: $font-size;
+    height: $button-height;
+    padding: 0 1em;
+    border-radius: $border-radius;
+    border: 1px solid $border-color;
+    background: $button-bg;
+    vertical-align: middle;
+    display: inline-flex;
+    justify-content: center;
+    align-content: center;
+    &:hover {
+      border-color: $border-color-hover;
+    }
+    &:active {
+      background-color: $button-active-bg;
+    }
+    &:focus {
+      outline: none;
+    }
+    > .icon {
+      order: 1;
+      margin-right: 0.3em;
+    }
+    > .g-button-content {
+      order: 2;
+      line-height: 15px;
+    }
+    &.icon-right {
+      > .icon {
+        order: 2;
+        margin-right: 0;
+        margin-left: 0.3em;
+      }
+      > .g-button-content {
+        order: 1;
+      }
+    }
+    .loading {
+      animation: spin 2s infinite linear;
+    }
+  }
 </style>
